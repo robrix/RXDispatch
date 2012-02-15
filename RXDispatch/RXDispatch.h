724ca3d2bdaa9f860de1a700930ed0329069bd5b
Rob Rix <rob@monochromeindustries.com>
2012-02-14 20:10:56 -0500

    Initial Commit

---
+++ RXDispatch/RXDispatch.h
@@ -0,0 +1,13 @@
+//
+//  RXDispatch.h
+//  RXDispatch
+//
+//  Created by Rob Rix on 12-02-14.
+//  Copyright (c) 2012 Monochrome Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RXDispatch : NSObject
+
+@end
---
+++ RXDispatchTests/RXDispatchTests.h
@@ -0,0 +1,13 @@
+//
+//  RXDispatchTests.h
+//  RXDispatchTests
+//
+//  Created by Rob Rix on 12-02-14.
+//  Copyright (c) 2012 Monochrome Industries. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RXDispatchTests : SenTestCase
+
+@end
