47dcc87e086acb1d9cae779766a09081edb945ce
Rob Rix <rob@monochromeindustries.com>
2012-02-14 20:28:26 -0500

    Adds a wrapper for dispatch_group_t.

---
+++ RXDispatch/RXDispatchGroup.h
@@ -0,0 +1,19 @@
+//  RXDispatchGroup.h
+//  Created by Rob Rix on 12-02-14.
+//  Copyright (c) 2012 Monochrome Industries. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface RXDispatchGroup : NSObject {
+@private
+	dispatch_group_t group;
+}
+
+-(id)initWithGroup:(dispatch_group_t)_group;
+
+@property (nonatomic, readonly) dispatch_group_t group;
+
+-(void)enter;
+-(void)leave;
+
+@end
---
+++ RXDispatch/RXDispatchGroup.m
@@ -0,0 +1,36 @@
+//  RXDispatchGroup.m
+//  Created by Rob Rix on 12-02-14.
+//  Copyright (c) 2012 Monochrome Industries. All rights reserved.
+
+#import "RXDispatchGroup.h"
+
+@implementation RXDispatchGroup
+
+-(id)initWithGroup:(dispatch_group_t)_group {
+	if((self = [super init])) {
+		dispatch_retain(_group);
+		group = _group;
+	}
+	return self;
+}
+
+-(void)dealloc {
+	dispatch_release(group);
+#if !__has_feature(objc_arc)
+	[super dealloc];
+#endif
+}
+
+
+@synthesize group;
+
+
+-(void)enter {
+	dispatch_group_enter(group);
+}
+
+-(void)leave {
+	dispatch_group_leave(group);
+}
+
+@end
